c4df5e47ffa3c5f3938c8026eea90153309c0c4c
Richard Hitier <richard.hitier@co-libri.org>
2015-06-12 19:21:36 +0200

    Show only data subdirs

--- common.php
+++ common.php
@@ -1,10 +1,22 @@
 <?php
 // show subdirs welcome page
-$dirs = array_filter(glob('*'), 'is_dir');
+$subdirs = array_filter(glob('*'), 'is_dir');
+$dirs = array();
+
+// guess if any MUTE_A files in such subdirs
+$files = array();
+foreach( $subdirs as $dir){
+    $found=array_filter(glob("$dir/Stabiliser/NET/MUTE*"),'is_file');
+    if ( 0 < count( $found )){
+        array_push($files, $found[0]);
+        array_push($dirs, $dir);
+    }
+}
 
 // stop if nothing
 if ( 0 == count( $dirs)){
     echo '<h2 class="alert">ajoutez les sous-répertoires afin d\'utiliser ce module</h2>';
     exit;
 }
+
 ?>
